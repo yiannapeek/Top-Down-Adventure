c59c98afc5753c28754704d06f3a9fed78997bc5
yiannapeek <yiannapeek@gmail.com>
2025-11-13 09:34:05 -0500

    Added Player Movement Script

---
+++ Top Down Adventure/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // FEATURE LIST
+    // Use WASD for movement
+    // need a variable for speed 
+    // We need to store the direction of our character
+    // need a variable for running
+    // Special movements example: rolling or dashing
+    // Climbing
+    // Mantling
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
