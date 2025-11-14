230972bc98ea5db9574a457a55e729fa4d5c79a9
yiannapeek <yiannapeek@gmail.com>
2025-11-14 09:35:41 -0500

    Created Character Movement

--- Top Down Adventure/Assets/Scripts/PlayerMovement.cs
+++ Top Down Adventure/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,16 +12,24 @@ public class PlayerMovement : MonoBehaviour
     // Climbing
     // Mantling
 
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
