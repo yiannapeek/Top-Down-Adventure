d755e94209768cda58ee0b1b8110219360493102
yiannapeek <yiannapeek@gmail.com>
2025-11-19 12:32:32 -0500

    Added & Scripted Idle/Walk Animations

--- Top Down Adventure/Assets/Scripts/PlayerMovement.cs
+++ Top Down Adventure/Assets/Scripts/PlayerMovement.cs
@@ -15,11 +15,13 @@ public class PlayerMovement : MonoBehaviour
     private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +32,16 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("IsWalking", true);
+        if (context.canceled)
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
         moveInput = context.ReadValue<Vector2>();
+
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
